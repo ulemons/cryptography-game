48760f5138200dbd1f103ef18f1f10a4edb1ddd0
Umberto Sgueglia <umberto.sgueglia@external.fcagroup.com>
2022-11-30 10:16:59 +0100

    fix first exercises unit test

--- exercise1.py
+++ exercise1.py
@@ -3,12 +3,12 @@ def encrypt(message, key):
 
 def decrypt(message, key):
     pass
-
+    
 assert encrypt("test",2) == "vguv"
-assert encrypt("encrypted message",42) == "CLAPWNRCB7KCQQ8EC"
-assert encrypt("This may result as very difficult message, also because as the length increase also the complexity to decrypt this message increases",144) == "Vjkuboc,btguwnvbcubxgt,bfkhhkewnvboguucig?bcnuqbdgecwugbcubvjgbngpivjbkpetgcugbcnuqbvjgbeqorngzkv,bvqbfget,rvbvjkuboguucigbkpetgcugu"
+assert encrypt("encrypted message",42) == "+I@MTKO+:7H+NN8B+"
+assert encrypt("This may result as very difficult message, also because as the length increase also the complexity to decrypt this message increases",144) == "PdeoWiXuWnaoqhpWXoWranuW ebbeZqhpWiaooXcawWXhokWYaZXqoaWXoWpdaWhajcpdWejZnaXoaWXhokWpdaWZkilhatepuWpkW aZnulpWpdeoWiaooXcaWejZnaXoao"
 
 
 assert decrypt("vguv",2) == "test"
-assert decrypt("CLAPWNRCB7KCQQ8EC",42) == "encrypted message"
-assert decrypt("Vjkuboc,btguwnvbcubxgt,bfkhhkewnvboguucig?bcnuqbdgecwugbcubvjgbngpivjbkpetgcugbcnuqbvjgbeqorngzkv,bvqbfget,rvbvjkuboguucigbkpetgcugu",144) == "This may result as very difficult message, also because as the length increase also the complexity to decrypt this message increases"
+assert decrypt("+I@MTKO+:7H+NN8B+",42) == "encrypted message"
+assert decrypt("PdeoWiXuWnaoqhpWXoWranuW ebbeZqhpWiaooXcawWXhokWYaZXqoaWXoWpdaWhajcpdWejZnaXoaWXhokWpdaWZkilhatepuWpkW aZnulpWpdeoWiaooXcaWejZnaXoao",144) == "This may result as very difficult message, also because as the length increase also the complexity to decrypt this message increases"
